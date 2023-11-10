7c50e9dbdcf338a1d2f3d79161a05c37d3720164
Anxhid-Mohed <mohammedanshid100@gmail.com>
2023-11-10 10:55:00 +0530

    first commit

--- src/components/practiceProject/PracticeProject.jsx
+++ src/components/practiceProject/PracticeProject.jsx
@@ -3,22 +3,25 @@ const PracticeProject = () => {
     <div className="flex flex-col mt-[7.5rem] gap-3 lg:flex-row ">
       <Card
         img="/puzzleIcon.svg"
-        heading="Practice Projects"
+        heading="Feature 1"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
       />
       <div className=" h-[2px] my-10 bg-gradient-to-r lg:my-0  lg:w-[2px] lg:h-auto  lg:bg-gradient-to-b from-[#d7cea500] via-[#d7cea5] to-[#d7cea500] "></div>
       <Card
         img="/tickbigIcon.svg"
-        heading="Assessments"
+        heading="Feature 2"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
       />
-      <div className=" h-[2px] my-10 bg-gradient-to-r lg:w-[1px] lg:my-0  lg:h-auto  lg:bg-gradient-to-b from-[#d7cea500] via-[#d7cea5] to-[#d7cea500]"></div>
+      {/* <div className=" h-[2px] my-10 bg-gradient-to-r lg:w-[1px] lg:my-0  lg:h-auto  lg:bg-gradient-to-b from-[#d7cea500] via-[#d7cea5] to-[#d7cea500]"></div> */}
+      <div className=" h-[2px] my-10 bg-gradient-to-r lg:my-0  lg:w-[2px] lg:h-auto  lg:bg-gradient-to-b from-[#d7cea500] via-[#d7cea5] to-[#d7cea500] "></div>
       <Card
         img="/startIcon.svg"
-        heading="Certificate of Completion"
+        heading="Feature 3"
         text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
       />
+      
     </div>
+    
   );
 };
 
